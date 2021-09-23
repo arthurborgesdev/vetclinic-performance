5f070764fc155522aa557ab08ad2b4445a89fbcb
Arthur Borges <arthurborges.dev@gmail.com>
2021-09-23 09:12:46 -0300

    Create separate file to handle the insertion of data

--- schema.sql
+++ schema.sql
@@ -55,10 +55,6 @@ INSERT INTO vets (name) VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie
 -- Column added by the exercise
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
--- Adding stuff 6 times to get analyze time >= 1000 ms
-INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
-insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
-
 -- index applied (first query)
 CREATE INDEX ON visits (animal_id);
 
